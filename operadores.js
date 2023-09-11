0a083ee8c4d95a4dfee3906a40cdb398864fca8d
Jesher <jesherjesher1@gmail.com>
2023-09-11 15:46:25 -0600

    04 Operadores

---
+++ operadores.js
@@ -0,0 +1,36 @@
+
+
+
+// Operadores Matemáticos
+// + - * / % **
+// += += *= /= %=
+console.log(10 + 10);
+console.log(10 - 10);
+console.log(10 / 10);
+console.log(10 * 10);
+console.log(10 % 10);
+console.log(10 ** 10); //potencia
+
+let num = 15;
+console.log(num += 5); // num = num +5
+console.log(num -= 5);
+console.log(num *= 5);
+console.log(num /= 5);
+console.log(num %= 5);
+
+// Operadores booleanos
+// > < >= <= !== ===
+console.log(1 > 2);
+console.log(1 < 2);
+console.log(1 >= 2);
+console.log(1 <= 2);
+console.log(1 !== 2);
+console.log(1 === 2);
+
+// Operadores Lógicos
+// and &&
+console.log(true && true);
+// or ||
+console.log(true || true);
+// not !
+console.log(!true);
